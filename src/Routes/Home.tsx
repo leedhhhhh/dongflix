7b921b24e8f5a367db1c132adc80dd1ecc765fae
leedhhhhh <86703459+leedhhhhh@users.noreply.github.com>
2022-09-09 15:58:26 +0900

    슬라이드쇼 기능 추가

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,10 +1,16 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImagePath } from "../utils";
 
+// 한줄 당 박스 개수
+const offset = 6;
+
 const Wrapper = styled.div`
   background-color: black;
+  padding-bottom : 200px
 `;
 
 const Loader = styled.div`
@@ -26,20 +32,68 @@ const Banner = styled.div<{ bgPhoto: string }>`
 `;
 
 const Title = styled.h2`
-  font-size: 48px;
-  margin-bottom: 20px;
+  font-size: 68px;
+  margin-bottom: 20px; ;
 `;
 
 const Overview = styled.p`
-  font-size: 26px;
+  font-size: 30px;
   width: 50%;
 `;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)<{ bgPhoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerWidth + 5,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.outerWidth - 5,
+  },
+};
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
+
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+
+  const increaseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1; // 배너용 영화 1개 제외
+      const maxIndex = Math.floor(totalMovies / offset) - 1; // index는 0부터 시작
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
+    }
+  }
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   return (
     <Wrapper>
       {isLoading ? (
@@ -47,6 +101,7 @@ function Home() {
       ) : (
         <>
           <Banner
+            onClick={increaseIndex}
             bgPhoto={makeImagePath(
               data?.results[0].backdrop_path
                 ? data?.results[0].backdrop_path
@@ -56,6 +111,24 @@ function Home() {
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
+              <Row 
+                variants={rowVariants} 
+                initial="hidden" 
+                animate="visible" 
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+                key={index}
+              >
+                {data?.results
+                  .slice(1)
+                  .slice(offset * index, offset * index + offset).map((movie) => (
+                  <Box key={movie.id} bgPhoto={makeImagePath(movie.backdrop_path, "w500")}/>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       )}
     </Wrapper>
